6311e8e7c5388fdfdea8473dfddcc5043901ed8c
windy <yafei@dao42.com>
2024-09-27 16:03:01 +0800

    lyf first

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    const greetings = ["你好！", "欢迎！", "很高兴见到你！", "祝你今天愉快！"];
+    let currentIndex = 0;
+
+    $("#changeGreeting").click(function() {
+        currentIndex = (currentIndex + 1) % greetings.length;
+        $("#greeting").text(greetings[currentIndex]);
+
+        // 添加一个简单的动画效果
+        $("#greeting").hide().fadeIn(500);
+    });
+});
