7a2272dd89552301a0583abd394fee376e11862e
clackypaas <clackypaas>
2024-11-08 14:10:08 +0800

    feat: upgrade jQuery to latest version and update script.js for compatibility

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     const greetings = ["你好！", "欢迎！", "很高兴见到你！", "祝你今天愉快！"];
     let currentIndex = 0;
 
-    $("#changeGreeting").click(function() {
+    $("#changeGreeting").on("click", function() {
         currentIndex = (currentIndex + 1) % greetings.length;
         $("#greeting").text(greetings[currentIndex]);
 
